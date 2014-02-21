ca0dc0a74897c979f30c1848c09ae6d9bbe244ed
Andrew Nguyen <atnguyen4@gmail.com>
2014-02-20 21:42:09 -0500

    Add status member to flashproxy.

--- chrome/background.js
+++ chrome/background.js
@@ -1,27 +1,20 @@
-main();
+var fp = new FlashProxy();
 
-function main() {
-    var fp = new FlashProxy();
-    fp.on_proxy_start = on_proxy_start;
-    fp.on_proxy_end = on_proxy_end;
-    fp.on_disable = on_disable;
-    fp.on_die = on_die;
-    fp.start();
+fp.on_proxy_start = function() {
+    chrome.browserAction.setIcon({ "path": "images/active_icon.png"});
+};
 
-    function on_proxy_start() {
-        chrome.browserAction.setIcon({ "path": "images/active_icon.png"});
-    }
+fp.on_proxy_end = function() {
+    // return to default icon
+    chrome.browserAction.setIcon({ "path": "images/cupcake48.png"});
+};
 
-    function on_proxy_end() {
-        // return to default icon
-        chrome.browserAction.setIcon({ "path": "images/cupcake48.png"});
-    }
+fp.on_disable = function() {
+    chrome.browserAction.setIcon({ "path": "images/disabled_icon.png"});
+};
 
-    function on_disable() {
-        chrome.browserAction.setIcon({ "path": "images/disabled_icon.png"});
-    }
+fp.on_die = function() {
+    chrome.browserAction.setIcon({ "path": "images/dead_icon.png"});
+};
 
-    function on_die() {
-        chrome.browserAction.setIcon({ "path": "images/dead_icon.png"});
-    }
-}
+fp.start();
