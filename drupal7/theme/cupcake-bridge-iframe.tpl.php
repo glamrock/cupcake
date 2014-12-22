39a0e070f7847204712f5206b0760dbec3850aa5
Griffin Boyce <griffin@cryptolab.net>
2014-12-22 04:18:43 -0500

    drupal 7 module

---
+++ drupal7/theme/cupcake-bridge-iframe.tpl.php
@@ -0,0 +1,9 @@
+<?php
+	/**
+	 * @file cupcake-bridge-iframe.tpl.php
+	 *
+	 * Provides an frame.
+	 *
+	 */
+?>
+<iframe src="//crypto.stanford.edu/flashproxy/embed.html<?php print ($iframe == 'cookierequired') ? '?cookierequired' : '';?>" width="80" height="15" frameborder="0" scrolling="no"></iframe>
